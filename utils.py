0060ce42e8dcce55bfe119dff4cd1a23f5440e16
Naveen Shankar Nagaraja <nagaraja@cs.uni-freiburg.de>
2019-12-29 23:42:56 +0530

    rename read authors func

--- utils.py
+++ utils.py
@@ -15,13 +15,13 @@ def author_details_as_dict(authors_details_list):
     return dict([x.split(':') for x in authors_details_list])
 
 
-def read_authors_file(authors_file):
+def read_authors_file_as_dict(authors_file):
     try:
         if not exists(authors_file):  raise IOError
         with open(authors_file, 'r') as f:
             contents = filter(lambda x: x.strip(), f.readlines())
         return author_details_as_dict(contents)
-    
+
     except IOError:
         print('[ERROR]: authors.txt not found, check README!')
         sys.exit(1)
