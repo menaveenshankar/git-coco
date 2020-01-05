ec82c9826be8d19fa16f71d08a282d6283cf1aae
Naveen Shankar Nagaraja <nagaraja@cs.uni-freiburg.de>
2020-01-05 15:01:12 +0530

    coauthors, issue number class derived from base

--- commit_message.py
+++ commit_message.py
@@ -4,3 +4,13 @@ from abc import ABC
 class CommitMessage(ABC):
     def __init__(self):
         self.message = ""
+
+
+class CoauthorsCommitMessage(CommitMessage):
+    def __init__(self):
+        super(CoauthorsCommitMessage, self).__init__()
+
+
+class IssueNumberCommitMessage(CommitMessage):
+    def __init__(self):
+        super(IssueNumberCommitMessage, self).__init__()
